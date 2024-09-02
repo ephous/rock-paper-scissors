a0f4253a01185611ec79e0f713912119cf6577df
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 09:55:10 -0400

    Add JS code to get computer and human choices
    The computer will randomly choose. The human will
    be prompted by the browser to make a choice. The
    choices will be printed to the console.

--- game-code.js
+++ game-code.js
@@ -1,2 +1,22 @@
+const choices = ['rock', 'paper', 'scissors'];
+
+let humanScore = 0;
+let computerScore = 0;
+
+// adopted from MDN but makes sense
+function getRandomInteger(maxval) {
+  return Math.floor( maxval * Math.random() );
+}
+
+function getComputerChoice(){
+    return choices[ getRandomInteger(3)];
+}
+
+function getHumanChoice(){
+    return prompt('Make your choice!');
+}
+
 // intitial test to make sure things are working
 console.log("Hello world!");
+console.log("You chose " + getHumanChoice() );
+console.log("Computer chose " + getComputerChoice() );
