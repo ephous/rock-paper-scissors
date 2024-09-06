1802f78ca70e64c656bb5c0700143557d0546edf
ephous <179286206+ephous@users.noreply.github.com>
2024-09-06 18:51:12 -0400

    Update console-based rock-paper-scissors game to use UI elements.
    Added buttons for the user to select which item to
    play in a given round. The choice is highlighted
    in yellow, and a simple text box shows the outcome
    of the round. A rudimentarty scoreboard keeps
    track of the scores, and when a player reaches 5,
    that player is declared the winner.

--- game-code.js
+++ game-code.js
@@ -36,43 +36,49 @@ function playRound( humanChoice, computerChoice) {
     } catch {
         return false;
     }
-
+    
+    let msg;
+    
     console.log('Computer: ' + computerChoice + ' vs You: ' + humanChoice )
     if(humanChoice == computerChoice){
-        console.log('No winner...you both chose ' + computerChoice )
+        msg = `No winner...you both chose ${computerChoice}`;
+        computerChoiceDiv.textContent = msg; // uses hoisting :)
         return true;
     }
 
     if(humanChoice=="rock"){
         if(computerChoice=='paper'){
-            console.log('Paper covers rock...you lose!');
+            msg = 'Paper covers rock...you lose!';
             computerScore++;
         } else {
-            console.log('Rock break scissors...you win!');
+            msg = 'Rock break scissors...you win!';
             humanScore++;
         }
+        computerChoiceDiv.textContent = msg; // uses hoisting :)
         return true;
     }
 
     if(humanChoice=="paper"){
         if(computerChoice=='rock'){
-            console.log('Paper covers rock...you win!');
+            msg = 'Paper covers rock...you win!';
             humanScore++;
         } else {
-            console.log('Scissors cut paper...you lose!');
+            msg = 'Scissors cut paper...you lose!';
             computerScore++;
         }
+        computerChoiceDiv.textContent = msg; // uses hoisting :)
         return true;
     }
 
     if(humanChoice=="scissors"){
         if(computerChoice=='rock'){
-            console.log('Rock breaks scissors...you lose!');
+            msg = 'Rock breaks scissors...you lose!';
             computerScore++;
         } else {
-            console.log('Scissors cut paper...you win!');
+            msg = 'Scissors cut paper...you win!';
             humanScore++;
         }
+        computerChoiceDiv.textContent = msg; // uses hoisting :)
         return true;
     }
     
@@ -81,6 +87,74 @@ function playRound( humanChoice, computerChoice) {
 
 }
 
+function updateScoreBoard(){
+  computerScoreBoard.textContent = computerScore;
+  humanScoreBoard.textContent = humanScore;
+}
+
+/* displays */
+const computerChoiceDiv = document.querySelector('#computer-selection')
+
+const computerScoreBoard = document.querySelector('#computer-score>a')
+const humanScoreBoard = document.querySelector('#human-score>a')
+
+
+/* listeners on each button
+const buttonPlayerRock = document.querySelector('#player-rock');
+buttonPlayerRock.addEventListener( "click", (e)=>{
+  console.log(e)
+  playRound("rock", getComputerChoice());
+  updateScoreBoard();
+});
+
+const buttonPlayerPaper = document.querySelector('#player-paper');
+buttonPlayerPaper.addEventListener( "click", (e)=>{
+  playRound("paper", getComputerChoice());
+  updateScoreBoard(); 
+});
+
+const buttonPlayerScissors = document.querySelector('#player-scissors');
+buttonPlayerScissors.addEventListener( "click", (e)=>{
+  playRound("scissors", getComputerChoice());
+  updateScoreBoard(); 
+});
+*/
+
+/* generic listener */
+const buttonContainer = document.querySelector('#player-buttons');
+buttonContainer.addEventListener( "click", (e)=>{
+  
+  // reset button colors
+  Array.from(buttonContainer.children).forEach( x=>{
+    x.style.backgroundColor='lightgray';
+  })
+  e.target.style.backgroundColor='yellow';
+  playRound(e.target.innerText.toLowerCase(), getComputerChoice());
+  updateScoreBoard();
+  if( (computerScore >=5) || (humanScore >=5) ){
+    
+    const scoreBoards = document.querySelector('#container-running-score');
+  
+    // make sure scoreboard is updated before prompting for another game
+    // inspired by: https://stackoverflow.com/questions/8840580
+    setTimeout(function() {
+      let msg = (computerScore >=5) ? 'The computer' : 'Congrats! You';
+      msg = `${msg} won best of five games! Play again?`;
+      if (confirm(msg)) {
+        computerScore=0;
+        humanScore=0;
+        updateScoreBoard();
+        return;
+      };
+    },0);
+    
+    // https://stackoverflow.com/questions/15555295/how-to-disable-div-element-and-everything-inside
+    buttonContainer.style.pointerEvents='none';
+  }
+});
+
+
+/*
 (function playGame(num_games = 5) {
   
   let playAnother = true;
@@ -98,3 +172,4 @@ function playRound( humanChoice, computerChoice) {
   console.log(`Final score: Computer: ${computerScore} .... You: ${humanScore} \n`);
 
 })() // play it right away...
+*/
